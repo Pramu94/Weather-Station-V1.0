4e35a821d1c11e551ea220b12024caf194c25477
Pramuditha <49787217+Pramu94@users.noreply.github.com>
2021-10-08 23:51:53 +0530

    Humidity and temperature
    display - serial monitor

--- Weather_Station_V1.0/Weather_Station_V1.0.ino
+++ Weather_Station_V1.0/Weather_Station_V1.0.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <dht11.h>
+#define DHT11PIN 4
 
+dht11 DHT11;
+
+void setup()
+{
+  Serial.begin(9600);
+ 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.println();
+
+  int chk = DHT11.read(DHT11PIN);
+
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Temperature (C): ");
+  Serial.println((float)DHT11.temperature, 2);
+
+  delay(2000);
 
 }
